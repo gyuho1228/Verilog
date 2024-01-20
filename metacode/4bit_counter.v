354a6b649100f8059151023a5e82fe040c2020c6
gyuho1228 <144807315+gyuho1228@users.noreply.github.com>
2024-01-20 20:49:28 +0900

    Create 4bit_counter.v

---
+++ metacode/4bit_counter.v
@@ -0,0 +1,40 @@
+module 4bit_counter(
+  input clk, rstn,
+  output reg [3:0] out
+);
+  always @(posedge clk, negedge rstn)
+    if(!rstn) out <= 0;
+    else      out <= out + 1;
+endmodule
+
+
+//test bench
+module tb;
+  
+  reg clk, rstn;
+  wire [3:0] out;
+  
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+  
+  initial begin
+    rstn = 1;
+    #10 rstn = 0;
+    #20 rstn = 1;
+  end
+  
+  initial begin
+    #300 $finish;
+  end
+  
+  4bit_counter u_4bit_counter(clk, rstn, out);
+  
+  initial begin
+    $dumpfile("4bit_counter.vcd");
+    $dumpvars(0, tb);
+  end            
+  
+endmodule
+
